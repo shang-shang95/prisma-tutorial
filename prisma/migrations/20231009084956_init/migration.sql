fce227ff033353dca81c2c063c6184887a36d00c
shang-shang95 <qqxn7ra9k@gmail.com>
2023-10-09 18:07:21 +0900

    fix(schema): add default values for createdAt and updatedAt

--- prisma/migrations/20231009084956_init/migration.sql
+++ prisma/migrations/20231009084956_init/migration.sql
@@ -3,7 +3,8 @@ CREATE TABLE "posts" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "body" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
 );
