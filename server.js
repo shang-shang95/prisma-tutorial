44492e6834efaa62136bc08c1c0b1ba5c9d1de1a
shang-shang95 <qqxn7ra9k@gmail.com>
2023-10-09 15:21:58 +0900

    feat(server): add /post endpoint for creating posts

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+import { Prisma, PrismaClient } from "@prisma/client";
 import express from "express";
 
 const app = express();
 const PORT = 8000;
+
+const prisma = new PrismaClient();
+
+app.post("/post", async (req, res) => {
+  const { title, body } = req.body;
+  const post = await prisma.post.create({
+    data: {
+      title,
+      body,
+    },
+  });
+  return res.json(post);
+});
 
 app.listen(PORT, () => {
   console.log("サーバーが起動中・・・");
