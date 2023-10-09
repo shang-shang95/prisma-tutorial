f3d46151b6251b4ce6fb9d005696cfab69f42bd5
shang-shang95 <qqxn7ra9k@gmail.com>
2023-10-09 13:15:24 +0900

    refactor: Rename "Posts" model to "Post"

--- prisma/migrations/20231009023952_init/migration.sql
+++ prisma/migrations/20231009023952_init/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
-CREATE TABLE "Posts" (
+CREATE TABLE "posts" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "body" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Posts_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
 );
